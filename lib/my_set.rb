46fce0ee082cb4c6b4163b3142d30de6d784d1b0
andrew <andrewasmit@hotmail.com>
2023-03-28 19:27:45 -0500

    commit tree 581ee378cfc81f37f747a5c3bb346a2724ec62bf

--- lib/my_set.rb
+++ lib/my_set.rb
@@ -1,1 +1,19 @@
-# your code here
+class MySet
+
+    def inititalize enumerable = [] 
+        @hash = {}
+        enumerable.each do |value|
+            @hash[value] = true
+        end
+    end
+
+    def include? value 
+        @hash.has_key?(value)
+    end
+
+    def add value
+        @hash[value] = true
+        self
+    end
+
+end
